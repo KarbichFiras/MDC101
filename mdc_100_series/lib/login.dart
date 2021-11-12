0c015aca7aca981b75193dcb94ca1c219c776248
KarbichFiras <firaskarbich@gmail.com>
2021-11-12 22:58:45 +0100

    textfields

--- mdc_100_series/lib/login.dart
+++ mdc_100_series/lib/login.dart
@@ -39,11 +39,27 @@ class _LoginPageState extends State<LoginPage> {
               ],
             ),
             const SizedBox(height: 120.0),
+            // [Name]
+            TextField(
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Username',
+              ),
+            ),
+            // spacer
+            const SizedBox(height: 12.0),
+            // [Password]
+            TextField(
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
             // TODO: Wrap Username with AccentColorOverride (103)
             // TODO: Remove filled: true values (103)
             // TODO: Wrap Password with AccentColorOverride (103)
-            // TODO: Add TextField widgets (101)
-            // TODO: Add button bar (101)
+            
           ],
         ),
       ),
