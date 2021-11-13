7dc57f7bde704e1900322ecd428afae41a18094f
KarbichFiras <firaskarbich@gmail.com>
2021-11-13 01:08:31 +0100

    Modify colors

--- mdc_100_series/lib/app.dart
+++ mdc_100_series/lib/app.dart
@@ -28,7 +28,7 @@ class ShrineApp extends StatelessWidget {
     return MaterialApp(
       title: 'Shrine',
       // TODO: Change home: to a Backdrop with a HomePage frontLayer (104)
-      home: const HomePage(),
+      home:  HomePage(),
       // TODO: Make currentCategory field take _currentCategory (104)
       // TODO: Pass _currentCategory for frontLayer (104)
       // TODO: Change backLayer field value to CategoryMenuPage (104)
@@ -55,30 +55,29 @@ class ShrineApp extends StatelessWidget {
 // TODO: Build a Shrine Theme (103)
 final ThemeData _kShrineTheme = _buildShrineTheme();
 
+
+
 ThemeData _buildShrineTheme() {
   final ThemeData base = ThemeData.light();
   return base.copyWith(
     colorScheme: base.colorScheme.copyWith(
-      primary: kShrinePink100,
-      onPrimary: kShrineBrown900,
-      secondary: kShrineBrown900,
+      primary: kShrinePurple,
+      secondary: kShrinePurple,
       error: kShrineErrorRed,
     ),
-    // TODO: Add the text themes (103)
-      textTheme: _buildShrineTextTheme(base.textTheme),
-      textSelectionTheme: const TextSelectionThemeData(
-        selectionColor: kShrinePink100,
-      ),
-    // TODO: Add the icon themes (103)
-    // TODO: Decorate the inputs (103)
+    scaffoldBackgroundColor: kShrineSurfaceWhite,
+    textTheme: _buildShrineTextTheme(base.textTheme),
+    textSelectionTheme: const TextSelectionThemeData(
+      selectionColor: kShrinePurple,
+    ),
     inputDecorationTheme: const InputDecorationTheme(
-      focusedBorder: OutlineInputBorder(
+      focusedBorder: CutCornersBorder(
         borderSide: BorderSide(
           width: 2.0,
-          color: kShrineBrown900,
+          color: kShrinePurple,
         ),
       ),
-        border: CutCornersBorder(),
+      border: CutCornersBorder(),
     ),
   );
 }
