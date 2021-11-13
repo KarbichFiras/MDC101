6b4c7e182ce08aa299840e39ca9a5cebfb7ff61b
KarbichFiras <firaskarbich@gmail.com>
2021-11-13 01:23:52 +0100

    Change home: to a Backdrop that has a HomePage as its frontLayer

--- mdc_100_series/lib/app.dart
+++ mdc_100_series/lib/app.dart
@@ -30,7 +30,16 @@ class ShrineApp extends StatelessWidget {
     return MaterialApp(
       title: 'Shrine',
       // TODO: Change home: to a Backdrop with a HomePage frontLayer (104)
-      home:  HomePage(),
+      home: Backdrop(
+        // TODO: Make currentCategory field take _currentCategory (104)
+        currentCategory: Category.all,
+        // TODO: Pass _currentCategory for frontLayer (104)
+        frontLayer: HomePage(),
+        // TODO: Change backLayer field value to CategoryMenuPage (104)
+        backLayer: Container(color: kShrinePink100),
+        frontTitle: Text('SHRINE'),
+        backTitle: Text('MENU'),
+      ),
       // TODO: Make currentCategory field take _currentCategory (104)
       // TODO: Pass _currentCategory for frontLayer (104)
       // TODO: Change backLayer field value to CategoryMenuPage (104)
