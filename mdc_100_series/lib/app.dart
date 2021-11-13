58a0f90c940414a57ca996a41a90ccefbc5004bd
KarbichFiras <firaskarbich@gmail.com>
2021-11-13 02:02:11 +0100

    Make currentCategory field take _currentCategory

--- mdc_100_series/lib/app.dart
+++ mdc_100_series/lib/app.dart
@@ -46,11 +46,14 @@ class _ShrineAppState extends State<ShrineApp> {
       // TODO: Change home: to a Backdrop with a HomePage frontLayer (104)
       home: Backdrop(
         // TODO: Make currentCategory field take _currentCategory (104)
-        currentCategory: Category.all,
+        currentCategory: _currentCategory,
         // TODO: Pass _currentCategory for frontLayer (104)
         frontLayer: HomePage(),
         // TODO: Change backLayer field value to CategoryMenuPage (104)
-        backLayer: Container(color: kShrinePink100),
+        backLayer: CategoryMenuPage(
+          currentCategory: _currentCategory,
+          onCategoryTap: _onCategoryTap,
+        ),
         frontTitle: Text('SHRINE'),
         backTitle: Text('MENU'),
       ),
