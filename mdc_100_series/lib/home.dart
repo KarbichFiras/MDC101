53a4ccd8596c3b4617ad9713f0767f1e882ef7f8
KarbichFiras <firaskarbich@gmail.com>
2021-11-13 02:03:16 +0100

    add a variable for Category and pass it to the AsymmetricView

--- mdc_100_series/lib/home.dart
+++ mdc_100_series/lib/home.dart
@@ -27,10 +27,12 @@ import 'supplemental/asymmetric_view.dart';
 
 class HomePage extends StatelessWidget {
   // TODO: Add a variable for Category (104)
+  final Category category;
 
+  const HomePage({this.category = Category.all, Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // TODO: Return an AsymmetricView (104)
-    return AsymmetricView(products: ProductsRepository.loadProducts(Category.all));
+    // TODO: Pass Category variable to AsymmetricView (104)
+    return AsymmetricView(products: ProductsRepository.loadProducts(category));
   }
 }
